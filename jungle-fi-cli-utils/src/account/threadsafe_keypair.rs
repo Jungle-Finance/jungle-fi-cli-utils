3574463ffe69d73785f56dc62ccc381207087fc6
eric <ebrightfield@gmail.com>
2022-09-22 22:03:43 -0600

    trying mutex instead of rwlock

--- jungle-fi-cli-utils/src/account/threadsafe_keypair.rs
+++ jungle-fi-cli-utils/src/account/threadsafe_keypair.rs
@@ -1,10 +1,10 @@
-use std::sync::{Arc, RwLock};
+use std::sync::{Arc, Mutex};
 use solana_program::pubkey::Pubkey;
 use solana_sdk::signature::{Signature, Signer, SignerError};
 
 /// [impl Signer] in a threadsafe way.
 pub struct ThreadsafeSigner<T: Signer> {
-    pub inner: Arc<RwLock<T>>,
+    pub inner: Arc<Mutex<T>>,
 }
 
 impl<T: Signer> Clone for ThreadsafeSigner<T> {
@@ -17,14 +17,14 @@ impl<T: Signer> Clone for ThreadsafeSigner<T> {
 
 impl<T: Signer> Signer for ThreadsafeSigner<T> {
     fn try_pubkey(&self) -> Result<Pubkey, SignerError> {
-        Ok(self.inner.read().unwrap().pubkey())
+        Ok(self.inner.lock().unwrap().pubkey())
     }
 
     fn try_sign_message(&self, message: &[u8]) -> Result<Signature, SignerError> {
-        self.inner.read().unwrap().try_sign_message(message)
+        self.inner.lock().unwrap().try_sign_message(message)
     }
 
     fn is_interactive(&self) -> bool {
-        self.inner.read().unwrap().is_interactive()
+        self.inner.lock().unwrap().is_interactive()
     }
 }
